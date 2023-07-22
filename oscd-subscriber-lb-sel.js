b636198ac18552114972719e4cd9f6746cdaeecd
danyill <danyill@users.noreply.github.com>
2023-07-22 23:10:42 +0000

    Deploying to gh-pages from @ danyill/oscd-subscriber-lb-sel@fcf26f69e63bf37b30deed3be4dbdd6b5021d99c 🚀

--- oscd-subscriber-lb-sel.js
+++ oscd-subscriber-lb-sel.js
@@ -6422,7 +6422,7 @@ function getNextSiblings(element, n) {
 function decodeIntAddr(intAddr) {
     const firstChar = intAddr.charAt(0);
     const secondChar = intAddr.charAt(1);
-    if (firstChar === 'I' && ['A', 'B', 'C'].includes(secondChar)) {
+    if (['I', 'V'].includes(firstChar) && ['A', 'B', 'C'].includes(secondChar)) {
         return `${firstChar}${intAddr.slice(2)}`;
     }
     return intAddr;
